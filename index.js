5e43532beeb39afc3d153e073dbbd9ea2388b2ed
Kyle <kyle.theaker@yahoo.com>
2021-04-10 22:29:32 -0700

    commit tree e741aef9c8d1ff0fccb9156929c6251b2dee07fd

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+
+
+
+//const cards = (["Ada","Brendan","Ali"], "birthday")
+const oilers = ([ 'Lisa', 'Kaitlin', 'Jan' ],'surprise')
+function writeCards(oilers) {
+    for (let i = 0; 
+        i < oilers.length; i++) {
+        console.log(`Thank you, ${oilers[i]}, for the wonderful ${oilers(1)} gift!`)
+        debugger;
+    }
+}
